1c2b38c64491534c3d6a0bc6d0bd65f5e9c63359
Prateek Madapurmath <prateekm33@gmail.com>
2016-05-26 19:41:46 -0700

    move files around to organize directory. update file paths in server.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 // import { Router, Route, Link, browserHistory } from 'react-router';
 
-// var Route = window.ReactRouter.Route;
+var Route = window.ReactRouter.Route;
 
 ReactDOM.render(
     <App />
