2edfd23c4c0f3022467c232346bcbbb95a40d726
John Jenson <john@Johns-MacBook-Pro-3.local>
2016-06-02 11:46:22 -0700

    add profile template

---
+++ public/components/Profile.js
@@ -0,0 +1,23 @@
+
+class Profile extends React.Component {
+  constructor() {
+    super();
+    };
+  }
+
+  render() {
+
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className="col-md-5"> User </div>
+          <div className="col-md-7"> Favorite Topics </div>
+        </div>
+      </div>
+    );
+
+  }
+
+}
+
+export default Profile;
