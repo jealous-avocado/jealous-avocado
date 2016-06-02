3537881371c8d3d14d42eab8055324be3e834da6
John Jenson <john@Johns-MacBook-Pro-3.local>
2016-06-02 12:57:35 -0700

    Change apply to call for userObj in main.js

--- public/main.js
+++ public/main.js
@@ -25340,7 +25340,7 @@
 	      var username = $('#username').val(); // --> grabs username input
 	      var password = $('#password').val();
 	
-	      var userObj = this.escape.apply(this, { username: username, password: password });
+	      var userObj = this.escape.call(this, { username: username, password: password });
 	
 	      $.post('/signin', userObj).done(function () {
 	        // window.location = '/'+;
